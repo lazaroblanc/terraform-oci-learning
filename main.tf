0336a5a9c5769a9bec1fac8a4dc5eeb568a61266
Lázaro Blanc <lazaroblanc@users.noreply.github.com>
2023-03-27 00:44:37 +0200

    complete third example from tutorial

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "oci" {
-  region              = "eu-frankfurt-1"
+  region              = var.region
   auth                = "SecurityToken"
   config_file_profile = "DEFAULT"
 }
@@ -15,14 +15,14 @@ provider "oci" {
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "10.0.11.0/24"
-  compartment_id = "ocid1.tenancy.oc1..aaaaaaaawsowxw2lhdss2yrtmrhty6qoz56iuy2q6rlawcrxqygrvf7bv5sq"
+  compartment_id = var.compartment_id
   display_name   = "My internal VCN"
 }
 
 resource "oci_core_subnet" "dev" {
   vcn_id                     = oci_core_vcn.internal.id
   cidr_block                 = "10.0.11.0/24"
-  compartment_id             = "ocid1.tenancy.oc1..aaaaaaaawsowxw2lhdss2yrtmrhty6qoz56iuy2q6rlawcrxqygrvf7bv5sq"
+  compartment_id             = var.compartment_id
   display_name               = "Dev subnet"
   prohibit_public_ip_on_vnic = true
   dns_label                  = "dev"

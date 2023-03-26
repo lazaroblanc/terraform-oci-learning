ce7519c3b6624a4dfac259839b252cf7c177d784
Lázaro Blanc <lazaroblanc@users.noreply.github.com>
2023-03-27 00:40:04 +0200

    complete second example from tutorial

--- main.tf
+++ main.tf
@@ -18,3 +18,12 @@ resource "oci_core_vcn" "internal" {
   compartment_id = "ocid1.tenancy.oc1..aaaaaaaawsowxw2lhdss2yrtmrhty6qoz56iuy2q6rlawcrxqygrvf7bv5sq"
   display_name   = "My internal VCN"
 }
+
+resource "oci_core_subnet" "dev" {
+  vcn_id                     = oci_core_vcn.internal.id
+  cidr_block                 = "10.0.11.0/24"
+  compartment_id             = "ocid1.tenancy.oc1..aaaaaaaawsowxw2lhdss2yrtmrhty6qoz56iuy2q6rlawcrxqygrvf7bv5sq"
+  display_name               = "Dev subnet"
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "dev"
+}
